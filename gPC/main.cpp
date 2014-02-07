615827412df902bb27c04fbe3974e39e485cfae2
马 征 <mazheng@mazhengdeMacBook-Pro.local>
2014-02-07 13:27:37 +0800

    Initial Commit

---
+++ gPC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gPC
+//
+//  Created by 马 征 on 14-2-7.
+//  Copyright (c) 2014年 马 征. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
